3320fc743e223d71078830e02b75d29826939006
Annongkhanh <nongkhanhan@gmail.com>
2023-07-04 02:42:55 +0700

    fix chain

--- src/configuration/Config.ts
+++ src/configuration/Config.ts
@@ -15,29 +15,29 @@ export const THEME_CONFIG = { initialColorMode: THEME_INITIAL_COLOR }
 export const INFURA_KEY = 'b842e61092284d098badb6f70cae2963'
 
 export const shardeumSphinxDapps =  {
-    readonly id: 8081,
-    readonly name: "Shardeum Sphinx Dapps",
-    readonly network: "shmSphinx",
-    readonly nativeCurrency: {
+     id: 8081,
+     name: "Shardeum Sphinx Dapps",
+     network: "shmSphinx",
+     nativeCurrency: {
          name: "SHARDEUM",
          symbol: "SHM",
          decimals: 18,
     },
     rpcUrls: {
          default: {
-            readonly http:  ["https://dapps.shardeum.org"],
+             http:  ["https://dapps.shardeum.org"],
         },
          public: {
-            readonly http:  ["https://dapps.shardeum.org"],
+             http:  ["https://dapps.shardeum.org"],
         }
     },
-    readonly blockExplorers: {
-        readonly default: {
-            readonly name: "Shardeum Explorer";
-            readonly url: "https://explorer-dapps.shardeum.org";
+     blockExplorers: {
+         default: {
+             name: "Shardeum Explorer";
+             url: "https://explorer-dapps.shardeum.org";
         },
     },
-    readonly testnet: true,
+     testnet: true,
 }as const satisfies Chain
 
 export const NETWORKS = [shardeumSphinxDapps, polygonMumbai]
